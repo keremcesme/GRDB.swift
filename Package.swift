695bb2bac85c35dced5b898613c1061fbffa76ee
Kerem Cesme <keremcesme@protonmail.com>
2026-06-27 20:57:16 +0300

    as

--- Package.swift
+++ Package.swift
@@ -1,39 +1,23 @@
 // swift-tools-version:6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import Foundation
 import PackageDescription
 
 let darwinPlatforms: [Platform] = [
-    .iOS,
-    .macOS,
-    .macCatalyst,
-    .tvOS,
-    .visionOS,
-    .watchOS,
+    .iOS, .macOS, .macCatalyst, .tvOS, .visionOS, .watchOS,
 ]
 var swiftSettings: [SwiftSetting] = [
     .define("SQLITE_ENABLE_FTS5"),
     .define("SQLITE_ENABLE_SNAPSHOT"),
-    // Not all Linux distributions have support for WAL snapshots.
     .define("SQLITE_DISABLE_SNAPSHOT", .when(platforms: [.linux])),
 ]
 var cSettings: [CSetting] = []
 var dependencies: [PackageDescription.Package.Dependency] = []
 
-// Don't rely on those environment variables. They are ONLY testing conveniences:
-// $ SQLITE_ENABLE_PREUPDATE_HOOK=1 make test_SPM
 if ProcessInfo.processInfo.environment["SQLITE_ENABLE_PREUPDATE_HOOK"] == "1" {
     swiftSettings.append(.define("SQLITE_ENABLE_PREUPDATE_HOOK"))
     cSettings.append(.define("GRDB_SQLITE_ENABLE_PREUPDATE_HOOK"))
 }
 
-// The SPI_BUILDER environment variable enables documentation building
-// on <https://swiftpackageindex.com/groue/GRDB.swift>. See
-// <https://github.com/SwiftPackageIndex/SwiftPackageIndex-Server/issues/2122>
-// for more information.
-//
-// SPI_BUILDER also enables the `make docs-localhost` command.
 if ProcessInfo.processInfo.environment["SPI_BUILDER"] == "1" {
     dependencies.append(.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"))
 }
@@ -45,19 +29,21 @@ swiftSettings.append(.define("SQLCipher"))
 
 let package = Package(
     name: "GRDB",
-    defaultLocalization: "en", // for tests
+    defaultLocalization: "en",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v7),
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v7),
     ],
     products: [
+        .library(name: "GRDBSQLite", targets: ["GRDBSQLite"]),
+        .library(name: "GRDBSQLCipher", targets: ["GRDBSQLCipher"]),
         .library(name: "GRDB", targets: ["GRDB"]),
         .library(name: "GRDB-dynamic", type: .dynamic, targets: ["GRDB"]),
     ],
     dependencies: dependencies,
     targets: [
+        .systemLibrary(
+            name: "GRDBSQLite",
+            providers: [.apt(["libsqlite3-dev"])]),
         .target(
             name: "GRDBSQLCipher",
             dependencies: [.product(name: "SQLCipher", package: "SQLCipher.swift")]
@@ -65,6 +51,7 @@ let package = Package(
         .target(
             name: "GRDB",
             dependencies: [
+                .target(name: "GRDBSQLite"),
                 .product(name: "SQLCipher", package: "SQLCipher.swift"),
                 .target(name: "GRDBSQLCipher"),
             ],
@@ -79,17 +66,10 @@ let package = Package(
             dependencies: ["GRDB"],
             path: "Tests",
             exclude: [
-                "CocoaPods",
-                "Crash",
-                "CustomSQLite",
-                "GRDBManualInstall",
+                "CocoaPods", "Crash", "CustomSQLite", "GRDBManualInstall",
                 "GRDBTests/Core/DatabasePool/getThreadsCount.c",
-                "Info.plist",
-                "Performance",
-                "SPM",
-                "Swift6Migration",
-                "generatePerformanceReport.rb",
-                "parsePerformanceTests.rb",
+                "Info.plist", "Performance", "SPM", "Swift6Migration",
+                "generatePerformanceReport.rb", "parsePerformanceTests.rb",
             ],
             resources: [
                 .copy("GRDBTests/Betty.jpeg"),
@@ -99,7 +79,6 @@ let package = Package(
             ],
             cSettings: cSettings,
             swiftSettings: swiftSettings + [
-                // Tests still use the Swift 5 language mode.
                 .swiftLanguageMode(.v5),
                 .enableUpcomingFeature("InferSendableFromCaptures"),
                 .enableUpcomingFeature("GlobalActorIsolatedTypesUsability"),
